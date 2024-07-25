73e13ec07d392f328e4a66f464ac5597a5cf3b5f
Eric Mendes Moreira <eric.moreira@alunoescolassociais.org.br>
2024-07-25 10:03:23 -0300

    Adiciona descrições aos códigos utilizados

---
+++ validação de email/email.js
@@ -0,0 +1,13 @@
+
+function checarEMAIL(){
+    if(document.forms[0].email.value == "" || 
+       document.forms[0].email.value.indexOf("@") == -1 ||
+       document.forms[0].email.value.indexOf(".") == -1){
+        alert("Por favor informe um email válido");
+        return false;
+       }else{
+        alert("Email informado");
+        document.getElementById("email").innerHTML = document.forms[0].email.value;
+        
+       }
+}
